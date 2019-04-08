6d6f0441b4c8763f7c7b259c3ae42893e7032ca0
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-08 11:45:32 +0300

    Add DBCustomConnection static class

---
+++ DBLab6/DBCustomConnection.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+namespace DBLab6
+{
+    public static class DBCustomConnection
+    {
+        private static readonly string ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=EducationalDepartment;Integrated Security=True";
+        private static readonly object Stub = new object();
+        private static SqlConnection connection;
+
+        public static SqlConnection GetConnection()
+        {
+            if (connection == null)
+            {
+                lock (Stub)
+                {
+                    if (connection == null)
+                    {
+                        connection = new SqlConnection(ConnectionString);
+                    }
+                }
+            }
+
+            return connection;
+        }
+    }
+}
