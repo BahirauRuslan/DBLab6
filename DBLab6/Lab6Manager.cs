aa0483b844e9ecdee987f6e03a5773f0e7454529
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-08 13:01:02 +0300

    Add Lab6Manager class

---
+++ DBLab6/Lab6Manager.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+
+namespace DBLab6
+{
+    public class Lab6Manager
+    {
+        private DataSetCreator _creator = new DataSetCreator();
+
+        public void SetFilterToDataView(DataView dataView, string filter)
+        {
+            dataView.RowFilter = filter;
+        }
+
+        public void SetSortToDataView(DataView dataView, string sort)
+        {
+            dataView.Sort = sort;
+        }
+
+        public DataView GetViewTable()
+        {
+            return new DataView(GetTable().Tables["Students"]);
+        }
+
+        public DataView GetViewTableWithCalculatedColumn()
+        {
+            var dataSet = GetTable();
+            AddTable(dataSet);
+            var relation = new DataRelation(
+                "StudMarks",
+                dataSet.Tables["Students"].Columns["Id"],
+                dataSet.Tables["MarkRecords"].Columns["StudentId"]);
+            dataSet.Relations.Add(relation);
+            var count = new DataColumn(
+             "Кол-во оценок", 
+             typeof(int), 
+             "COUNT(Child(StudMarks).StudentId)");
+            dataSet.Tables["Students"].Columns.Add(count);
+            return new DataView(dataSet.Tables["Students"]);
+        }
+
+        public void AddTable(DataSet dataSet)
+        {
+            _creator.AddTableToDataSet(
+                dataSet,
+                DBCustomConnection.GetConnection(),
+                "SELECT Id, StudentId FROM MarkRecords", 
+                "MarkRecords");
+        }
+
+        public DataSet GetTable()
+        {
+            return _creator.CreateDataSet(
+                DBCustomConnection.GetConnection(), 
+                "SELECT Id, FullName, Number FROM Students JOIN Groups ON Students.GroupId = Groups.Id", 
+                "Students");
+        }
+    }
+}
