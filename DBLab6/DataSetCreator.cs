b708ff8979155cf3b00133c1b655a7493c11b94e
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-08 12:19:13 +0300

    Add DataSetCreator class

---
+++ DBLab6/DataSetCreator.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DBLab6
+{
+    public class DataSetCreator
+    {
+        public DataSet CreateDataSet(SqlConnection connection, string command, string tableName)
+        {
+            var adapter = new SqlDataAdapter(command, connection);
+            var dataSet = new DataSet();
+            adapter.Fill(dataSet, tableName);
+            return dataSet;
+        }
+
+        public void AddTableToDataSet(DataSet dataSet, SqlConnection connection, string command, string tableName)
+        {
+            var adapter = new SqlDataAdapter(command, connection);
+            adapter.Fill(dataSet, tableName);
+        }
+    }
+}
